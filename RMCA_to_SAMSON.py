e6a784bce06ccf91c46374a0482a883b31f6eb7f
kamitoteles <cama9709@gmail.com>
2021-03-28 15:24:35 -0500

    Directory actualization

--- RMCA_to_SAMSON.py
+++ RMCA_to_SAMSON.py
@@ -2,7 +2,7 @@
 ### Autor: Camilo Moreno - cama9709@gmail.com
 
 #// Importacion de librerias ////////////////////////////////////////////////////////////////////#
-import pandas as pd             # Version 1.1.0
+import pandas as pd             # Version 1.1.0 
 import numpy as np              # Version 1.19.1
 import os
 import fileinput
@@ -15,8 +15,32 @@ import fileinput
 #////////////////////////////////////////////////////////////////////////////////////////////////#
 station_code = '44444'
 station_name = 'KENNEDY'
-station_location = 'CO -5  N 4 39  W 74 5'
+station_location = 'CO -5  N 4 39  W 74 5' # COUNTRY_CODE UTZ_TIME N NORTH_COORD W WEST_COORD .. LATLON COORDS
 
+var_dic = {
+
+    '01': {'desc':'Extraterrestrial horizontal radiation'  , 'nan':'9999'},
+    '02': {'desc':'Extraterrestial direct normal radiation', 'nan':'9999'},
+    '03': {'desc':'Global horizontal radiation'            , 'RMCA_name':'Rad Solar', 'nan':'9999', 'extra':' ?0'},
+    '04': {'desc':'Direct normal radiation'                , 'nan':'9999', 'extra':' ?0'},
+    '05': {'desc':'Diffuse horizontal radiation'           , 'nan':'9999', 'extra':' ?0'},
+    '06': {'desc':'Total cloud cover'                      , 'nan':' 5'},
+    '07': {'desc':'Opaque cloud cover'                     , 'nan':' 5'},
+    '08': {'desc':'Dry bulb temperature'                   , 'RMCA_name':'Temperatura', 'nan':'9999.'},
+    '09': {'desc':'Dew point temperature'                  , 'nan':'9999.'},
+    '10': {'desc':'Relative humidity'                      , 'RMCA_name':'HR', 'nan':'999'},
+    '11': {'desc':'Station pressure'                       , 'RMCA_name':'Presion Baro', 'nan':'9999'},
+    '12': {'desc':'Wind direction'                         , 'RMCA_name':'Dir Viento', 'nan':'999'},
+    '13': {'desc':'Wind speed'                             , 'RMCA_name':'Vel Viento', 'nan':'9999.'},
+    '14': {'desc':'Visibility'                             , 'nan':'99999.'},
+    '15': {'desc':'Ceiling height'                         , 'nan':'999999'},
+    '16': {'desc':'Present weather'                        , 'nan':'999999999'},
+    '17': {'desc':'Precipitable water'                     , 'RMCA_name':'Precipitacion', 'nan':'9999'},
+    '18': {'desc':'Broadband aerosol optical depth'        , 'nan':'99999.'},
+    '19': {'desc':'Snow depth'                             , 'nan':'9999'},
+    '20': {'desc':'Days since last snowfall'               , 'nan':'999'},
+    '21': {'desc':'Hourly precipitation amount and flag'   , 'nan':' ' * 7},
+}
 #// Ingreso de direccion de archivo de meteorologia formato RMCAB 2020 //////////////////////////#
 dir_meteorologia = ''
 is_file = False
@@ -58,6 +82,7 @@ archivo_meteorologia = pd.read_excel(dir_meteorologia,
 #// Reasignacion de nombre de variables meteorlogicas segun formato SAMSON //////////////////////# 
 headers = [*archivo_meteorologia]
 for i in range(len(headers)):
+    
     if headers[i] == 'Unnamed: 0':      headers[i] = '00'
     elif headers[i] == 'Rad Solar':     headers[i] = '03'
     elif headers[i] == 'Temperatura':   headers[i] = '08'
@@ -72,14 +97,32 @@ archivo_meteorologia.columns = headers
 del archivo_meteorologia['No']
 
 #// Funcion de actualizacion de espacios en blanco de los valores ///////////////////////////////#
-def actualizar_espacios(i, columna, campos, nada, extra = ''):
-    if columna in archivo_meteorologia.columns:
-        if pd.isna(archivo_meteorologia[columna][i]):
-            archivo_meteorologia[columna][i] = nada
-        elif len(archivo_meteorologia[columna][i]) < campos:
-            temporary = archivo_meteorologia[columna][i]
-            space = campos - len(archivo_meteorologia[columna][i])
-            archivo_meteorologia[columna][i] = ' ' * space + temporary + extra
+def actualizar_espacios(i):
+    
+    num_rows = len(archivo_meteorologia['00'])
+    
+    for columna in list(var_dic):
+        
+        nada = var_dic[columna]['nan']
+            
+        try:
+            extra = var_dic[columna]['extra'] 
+        except:
+            extra = ''
+        
+        if columna in archivo_meteorologia.columns:
+            
+            if pd.isna(archivo_meteorologia[columna][i]):
+                archivo_meteorologia[columna][i] = nada + extra
+                
+            else:
+                temporary = archivo_meteorologia[columna][i]
+                space = len(nada) - len(temporary)
+                archivo_meteorologia[columna][i] = ' ' * space + temporary + extra
+        
+        elif i == num_rows-1: 
+            archivo_meteorologia[columna] = nada + extra
+            
 
 #// Conversion de formato de valores ////////////////////////////////////////////////////////////#
 year = []; month = []; day = []; hour = []
@@ -103,29 +146,7 @@ for i in range(len(archivo_meteorologia['00'])):
     else:
         hour.append(archivo_meteorologia['00'][i][11:13])
     
-    actualizar_espacios(i, '03', 4, '9999 ?0', ' ?0')
-    actualizar_espacios(i, '08', 5, '9999.')
-    actualizar_espacios(i, '10', 3, '999')
-    actualizar_espacios(i, '11', 4, '9999')
-    actualizar_espacios(i, '12', 3, '999')
-    actualizar_espacios(i, '13', 5, '9999.')
-    actualizar_espacios(i, '17', 4, '9999')
-    
-archivo_meteorologia['01'] = '9999'
-archivo_meteorologia['02'] = '9999'
-archivo_meteorologia['04'] = '9999 ?0'
-archivo_meteorologia['05'] = '9999 ?0'
-archivo_meteorologia['06'] = ' 5'
-archivo_meteorologia['07'] = ' 5'
-archivo_meteorologia['09'] = '9999.'
-archivo_meteorologia['11'] = '9999'
-archivo_meteorologia['14'] = '99999.'
-archivo_meteorologia['15'] = '999999'
-archivo_meteorologia['16'] = '999999999'
-archivo_meteorologia['18'] = '99999.'
-archivo_meteorologia['19'] = '9999'
-archivo_meteorologia['20'] = '999'
-archivo_meteorologia['21'] = ' ' * 7
+    actualizar_espacios(i)
 
 headers = [*archivo_meteorologia]
 archivo_meteorologia.columns = headers
@@ -166,3 +187,4 @@ print(f'==CONVERSION DE FORMATOS COMPLETA!==')
 print(f'====================================')
 print(f'Archivo RMCAB: {dir_meteorologia}')
 print(f'Archivo SAMSON: {dir_samson + "/" + name_samson}')
+
